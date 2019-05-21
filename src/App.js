86194a3328f95cfa2565f7b366672b86e6b11070
Bonggt <kepy1106@gmail.com>
2019-05-21 23:21:00 +0900

    Chapter.15(State)

--- src/App.js
+++ src/App.js
@@ -5,10 +5,21 @@ import TOC from "./components/TOC";
 import Content from "./components/Content";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    // Component의 값을 초기화
+    this.state = {
+      subject: {
+        title: "WEB",
+        sub: "World Wide Web!"
+      }
+    };
+  }
   render() {
+    const { subject } = this.state;
     return (
       <>
-        <Subject title="WEB" sub="world wide web!" />
+        <Subject title={subject.title} sub={subject.sub} />
         <TOC />
         <Content title="HTML" desc="HTML is Hyper Text Markup Language." />
       </>
